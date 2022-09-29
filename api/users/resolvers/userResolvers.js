ddb84a627714a9f5fec7074ac16454aa7fa72edd
Amós Silas <silastj@hotmail.com>
2022-09-28 21:59:15 -0300

    add more schema, url

--- api/users/resolvers/userResolvers.js
+++ api/users/resolvers/userResolvers.js
@@ -4,10 +4,6 @@ const useResolvers = {
     users: (root, args, {dataSources}) => dataSources.usersAPI.getUsers(),
     user: (root, {id}, {dataSources}) => dataSources.usersAPI.getUserById(id)
   }
-  // Query: {
-  //   roles: ({dataSources}) => dataSources.usersAPI.getRoles()
-  // }
 }
-
 
 module.exports = useResolvers
